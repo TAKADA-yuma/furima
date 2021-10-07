639737f70692786391243a0495747a866279936c
TAKADA-yuma <89758513+TAKADA-yuma@users.noreply.github.com>
2021-10-07 18:35:36 +0900

    商品出品機能　完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,16 +5,18 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :fee
   belongs_to :days
+  belongs_to :user
+
   has_one_attached :image
 
 
   validates :name, presence: true
   validates :exp, presence: true
   validates :image, presence: true
-  validates :price, presence: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/}
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :fee_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :days_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :fee_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :days_id, numericality: { other_than: 0, message: "can't be blank" }
 end
