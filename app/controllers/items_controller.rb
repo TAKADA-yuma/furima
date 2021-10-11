08f5d0f6a873d82fab1c0e416e4d8ec1948ea7b7
TAKADA-yuma <89758513+TAKADA-yuma@users.noreply.github.com>
2021-10-11 11:39:46 +0900

    編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
-
+  before_action :authenticate_user!, only:[:new,:edit]
+  before_action :set_item, only: [:show,:edit,:update]
+  before_action :another_user, only: [:edit]
 
   def index
     @items = Item.order("created_at DESC")
@@ -11,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -21,13 +21,31 @@ class ItemsController < ApplicationController
 
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
 private
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :image, :price, :exp, :category_id, :status_id, :fee_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
   end
   
+  def another_user
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
+  end
 end
