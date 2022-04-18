a4bff7c07782213dbafc6f3d46d80b9da9c1f7d8
TAKADA-yuma <89758513+TAKADA-yuma@users.noreply.github.com>
2022-04-18 11:49:30 +0900

    複数枚投稿

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,12 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :shipment
 
-  has_one_attached :image
+  has_many_attached :images
 
 
   validates :name, presence: true
   validates :exp, presence: true
-  validates :image, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id, numericality: { other_than: 0, message: "を選択して下さい"}
   validates :prefecture_id, numericality: { other_than: 0, message: "を選択して下さい"}
