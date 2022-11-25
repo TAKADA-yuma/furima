455d37a87726ac347a024bb980bc4e64d403fffa
TAKADA-yuma <89758513+TAKADA-yuma@users.noreply.github.com>
2022-11-25 15:30:54 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
